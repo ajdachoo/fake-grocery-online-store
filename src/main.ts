22b8a087cb2f6e647e52142a1ec3dc53f3530d23
Adam Cieslak <adamcieslak00@gmail.com>
2023-04-17 19:41:41 +0200

    disabled product quantity input

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ const renderProducts = (items: Product[]) => {
                 <img src="${items[i].image}" alt="${items[i].name}" class="item-icon">
                 <h1 class="product-title">${items[i].name}</h1>
                 <p class="product-description">${items[i].description}</p>
-                <div class="product-quantity"><div class="product-quantity-counter"><button data-id="${items[i].id}" value="-" disabled="disabled">-</button><input data-id="${items[i].id}" type="number" max="100" min="${minQty}" step="${minQty}" value="${minQty.toFixed(1).toString()}"><button data-id="${items[i].id}" value="+">+</button></div><span class="product-measure">${items[i].measure}</span></div>
+                <div class="product-quantity"><div class="product-quantity-counter"><button data-id="${items[i].id}" value="-" disabled="disabled">-</button><input data-id="${items[i].id}" disabled="disabled" type="number" max="100" min="${minQty}" step="${minQty}" value="${minQty.toFixed(1).toString()}"><button data-id="${items[i].id}" value="+">+</button></div><span class="product-measure">${items[i].measure}</span></div>
                 <div class="product-price"><span class="product-price-default">$${items[i].price.toFixed(2)}</span><span class="product-price-sale">${items[i].saleAmount ? "$" + items[i].saleAmount.toFixed(2) : ""}</span></div>
                 <button class="add-to-cart-button" data-id="${items[i].id}">Add to cart</button>`;
 
